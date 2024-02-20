7b9bc539a324dbf1eba99bbd20e3d8f9e0f3d935
Marie Knowlton <mtknowlton67@gmail.com>
2024-02-20 12:16:40 -0800

    Second Commit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,14 +1,16 @@
-
+import React, { Component } from 'react';
+import Hello from './components/Hello.js'
 import './App.css';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Hello/>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- my-portfolio/src/components/Hello.js
+++ my-portfolio/src/components/Hello.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
-function
+function Hello () {
+  return (
+    <h1>Hello World</h1>
+  )
+}
+
+export default Hello
